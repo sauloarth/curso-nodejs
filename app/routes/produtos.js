45a0f7e83e7a5f75c5878297b84527b32910a939
Saulo Reis <sauloarth@gmail.com>
2018-07-10 13:27:08 -0300

    Corrigindo bug  na definição das views

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,6 +1,6 @@
 module.exports = function(app) {
     app.get('/produtos', function(req, res) {
-        res.render('/produtos/lista');
+        res.render('produtos/lista');
         console.log('listando produtos ...');
     })
 }
--- config/express.js
+++ config/express.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.set('view engine', 'ejs');
-app.set('views', '/app/views');
+app.set('views', './app/views');
 
 module.exports = function() {
     console.log('Carregando express...')

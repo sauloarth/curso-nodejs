f70aa460608a9cd81c948c9438babb3651c0c329
Saulo Reis <sauloarth@gmail.com>
2018-07-10 18:19:24 -0300

    Isolando lógica de acesso ao banco em produtosBanco.js

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,8 +1,9 @@
 module.exports = function(app) {
     app.get('/produtos', function(req, res) {
         var connection = app.infra.connectionFactory();
+        var produtosBanco = app.infra.produtosBanco;
 
-        connection.query('select * from livros', function(err, results) {
+        produtosBanco.lista(connection, function(err, results) {
             res.render('produtos/lista', { lista: results });
         });
 

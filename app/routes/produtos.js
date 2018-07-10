1afea5bcdb3a9023c50fb3fc660486f774eec3b0
Saulo Reis <sauloarth@gmail.com>
2018-07-10 17:16:10 -0300

    Isolando a coneção com o banco em infra/connectionFactory

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,12 +1,7 @@
+var dbConnection = require('../infra/connectionFactory');
 module.exports = function(app) {
     app.get('/produtos', function(req, res) {
-        var mysql = require('mysql');
-        var connection = mysql.createConnection({
-            host: 'localhost',
-            user: 'root',
-            password: 'mysql',
-            database: 'curso-nodejs'
-        });
+        var connection = dbConnection();
 
         connection.query('select * from livros', function(err, results) {
             res.render('produtos/lista', { lista: results });

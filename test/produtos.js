c1f41d216ba840f4e5c90e89acbe0ea820446352
Saulo Reis <sauloarth@gmail.com>
2018-07-11 20:28:42 -0300

    Casos de teste de produto válido e invalido.

--- test/produtos.js
+++ test/produtos.js
@@ -8,4 +8,23 @@ describe('#ProdutosController', function() {
             .expect('Content-type', /json/)
             .expect(200, done);
     })
+
+    it('cadastro de produto com dados invalidos', function(done) {
+        request.post('/produtos')
+            .send({
+                descricao: 'string valida'
+            })
+            .expect(400, done);
+    })
+
+    it('cadastro de produto com dados validos', function(done) {
+        request.post('/produtos')
+            .send({
+                titulo: 'string valida',
+                descricao: 'string valida',
+                preco: 1.23
+            })
+            .expect(302, done);
+    })
+
 })

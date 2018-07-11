cd31a1eb5889db9d279d10e2d4991827d32cf923
Saulo Reis <sauloarth@gmail.com>
2018-07-11 08:24:38 -0300

    Passando a conexão para a função produtosBanco para que não precise ser encaminhada em cada requisição ao banco.

--- app/infra/produtosBanco.js
+++ app/infra/produtosBanco.js
@@ -1,7 +1,15 @@
 module.exports = function() {
-    this.lista = function(connection, callback) {
-        connection.query('select * from livros', callback);
+
+    /* Como o express load já chama a função automaticamente quando carrega
+     * faço um wrapper que retorna outra função que será chamada oportunamente.
+     */
+
+    return function(connection) {
+
+        this.lista = function(callback) {
+            connection.query('select * from livros', callback);
+        }
+        return this;
 
     }
-    return this;
 }

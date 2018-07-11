ef971599587c7b86b0d431a40619d29777225ba6
Saulo Reis <sauloarth@gmail.com>
2018-07-11 11:49:35 -0300

    Configurando body-parser para importar informações da requisição com express.

--- config/express.js
+++ config/express.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var load = require('express-load');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
+
+//middleware
+app.use(bodyParser.urlencoded({ extended: true }));
 
 load('routes', { cwd: 'app' })
     .then('infra')

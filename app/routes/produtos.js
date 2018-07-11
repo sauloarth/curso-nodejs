db0ee2a7532f147bc2e0498e6ed4358e7835c756
Saulo Reis <sauloarth@gmail.com>
2018-07-11 08:28:57 -0300

    Força a criação de novo contexto para produtosBanco.

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,7 +1,7 @@
 module.exports = function(app) {
     app.get('/produtos', function(req, res) {
         var connection = app.infra.connectionFactory();
-        var produtosBanco = app.infra.produtosBanco(connection);
+        var produtosBanco = new app.infra.produtosBanco(connection);
 
         produtosBanco.lista(function(err, results) {
             res.render('produtos/lista', { lista: results });

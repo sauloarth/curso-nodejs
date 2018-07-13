fa68a03f38f9a5d8371de83f37afeda4742d2a28
Saulo Reis <sauloarth@gmail.com>
2018-07-12 22:00:28 -0300

    Testando configuração de middleware

--- config/express.js
+++ config/express.js
@@ -23,14 +23,7 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.use(function(error, req, res, next) {
-    if (process.env.NODE_ENV == 'production') {
-        res.status(500).render('erros/500');
-        return;
-    }
-    next(error);
 
-})
 
 module.exports = function() {
     console.log('Carregando express...')
